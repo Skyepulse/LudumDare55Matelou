fe81862a6318a78fa94bcfa38f0aaecb0a3000ee
ippieh <pierina100a@gmail.com>
2024-04-13 19:20:10 +0200

    pentagram appears on command and reacts to player walking to it

---
+++ Scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+#to create some pentagrams in the scene
+
+var pentagram = preload("res://Scenes/pentagram_scene.tscn")
+
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("test_pent"):
+		inst(Vector2(get_global_mouse_position()))
+
+	
+func inst(pos : Vector2) -> void :
+	print("instantiating")
+	var instance = pentagram.instantiate()
+	instance.position = pos
+	add_child(instance)
+
---
+++ Scripts/pentagram.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var sprite : Sprite2D = $Sprite2D
+#var tileMap = preload("res://Scenes/tileMap.tscn")
+
+func _ready() -> void:
+	#connect the body and the pentagram
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	#when player enters pentagram do something and spin the pentagram away
+	var tween = create_tween()
+	tween.tween_property(sprite, "position:y", -20.0, .5)
+	tween.parallel().tween_property(sprite, "rotation_degrees", -360.0, .5)
+	tween.tween_callback(queue_free)
+
