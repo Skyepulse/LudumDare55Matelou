b88a49d5eca1d15a4ed32e96b3cc2f429061dec6
ippieh <pierina100a@gmail.com>
2024-04-14 15:24:58 +0200

    adding features on pentagram spawn

--- Scripts/pentagram_spawner.gd
+++ Scripts/pentagram_spawner.gd
@@ -23,34 +23,45 @@ func _physics_process(delta):
 		var pos = player.global_position
 		inst(pos)
 		
-func random_pos(position):
-	#select a random cell around the player
-	var randomx = rng.randf_range(-128.0, 128.0)
-	#make sure it doesn't spawn on the player
+func random_pos(position, pentagram_size):
+	var randomx = 0.0
+	var randomy = 0.0
+	var safety_diameter = 200.0
+	var spawn_range = 256.0
+	if (pentagram_size == "big"):
+		safety_diameter = 350.0
+		spawn_range = 400.0
+	while (Vector2(position.x+randomx,position.y+randomy).distance_to(position)<200.0):
+		#select a random cell around the player
+		randomx = rng.randf_range(-spawn_range, spawn_range)
+		randomy = rng.randf_range(-spawn_range, spawn_range)
 	
-	var randomy = rng.randf_range(-128.0, 128.0)
 	#make sure it doesn't spawn on the player
-	
 	print (Vector2(position.x+randomx, position.y+randomy), position)
 	return Vector2(position.x+randomx, position.y+randomy)
 	
 func inst(pos : Vector2) -> void :
-	#generate random position around player
-	var rand_pos = random_pos(pos)
 	#choose big or small pentagram with 1/10 odds
-	var rand_pentagram_size = rng.randi_range(0,10)
+	var rand_pentagram_size = rng.randi_range(0,11)
+	print(rand_pentagram_size)
 	
-	#instantiate the pentagram
 	if rand_pentagram_size<10:
+			#generate random position around player
+		var rand_pos = random_pos(pos,"small")
+		
+			#instantiate the pentagram
 		var instance = pentagram.instantiate()
 		instance.position = rand_pos
 		add_child(instance)
 	else:
+			#generate random position around player
+		var rand_pos = random_pos(pos,"big")
+		
+			#instantiate the pentagram
 		var instance = big_pentagram.instantiate()
 		instance.position = rand_pos
+		print("intstance big")
 		add_child(instance)
-		
-# Called when the node enters the scene tree for the first time.
 
 
 func _on_timer_timeout():
