8515d3b469f56769ce4a97da3e24f0e9510df554
Telo <telo.philippe@telecom-paris.fr>
2024-04-14 15:13:17 +0200

    Changed npc

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,13 +1,9 @@
 extends Node2D
-var dialog = "Hello! This is a test dialog from"
 
-var sceneCanvasLayer:CanvasLayer = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sceneCanvasLayer = get_tree().get_root().get_child(0).get_node("SceneCanvasLayer") as CanvasLayer
-	if sceneCanvasLayer == null: print("CanvasLayer not found!!")
-	dialog += " " + str(name)
-
+	pass
+	
 func talk():
-	sceneCanvasLayer.get_node("NPC_TALK_VIEW").talk_dialog(dialog, name)
+	Dialogic.start("AzazaelMeet2")
 
