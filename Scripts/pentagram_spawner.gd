00e3aeee3610cb3b0677f06d080afa0a8ec18cc7
ippieh <pierina100a@gmail.com>
2024-04-14 13:59:41 +0200

    added big pentagram instatiation

--- Scripts/pentagram_spawner.gd
+++ Scripts/pentagram_spawner.gd
@@ -5,6 +5,7 @@ var frequency = 2.0
 #to create some pentagrams in the scene
 
 var pentagram = preload("res://Scenes/pentagram_scene.tscn")
+var big_pentagram = preload("res://Scenes/pentagram_scene.tscn")
 @onready var tileMap : TileMap =$'../mapMainNode/TileMap'
 @onready var player = $'../CharacterBody2D'
 var rng = RandomNumberGenerator.new()
@@ -26,31 +27,29 @@ func random_pos(position):
 	#select a random cell around the player
 	var randomx = rng.randf_range(-128.0, 128.0)
 	#make sure it doesn't spawn on the player
-	if randomx<0:
-		randomx-=240
-		print(randomx)
-		print(position.x)
-	else:
-		print(randomx)
-		print(position.x)
-		randomx+=240
+	
 	var randomy = rng.randf_range(-128.0, 128.0)
 	#make sure it doesn't spawn on the player
-	if randomy<0:
-		randomx-=240
-	else:
-		randomy+=240
+	
 	print (Vector2(position.x+randomx, position.y+randomy), position)
 	return Vector2(position.x+randomx, position.y+randomy)
 	
 func inst(pos : Vector2) -> void :
 	#generate random position around player
 	var rand_pos = random_pos(pos)
+	#choose big or small pentagram with 1/10 odds
+	var rand_pentagram_size = rng.randi_range(0,10)
 	
 	#instantiate the pentagram
-	var instance = pentagram.instantiate()
-	instance.position = rand_pos
-	add_child(instance)
+	if rand_pentagram_size<10:
+		var instance = pentagram.instantiate()
+		instance.position = rand_pos
+		add_child(instance)
+	else:
+		var instance = big_pentagram.instantiate()
+		instance.position = rand_pos
+		add_child(instance)
+		
 # Called when the node enters the scene tree for the first time.
 
 
