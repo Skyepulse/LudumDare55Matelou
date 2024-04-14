a1067edd00764c6fb52cc632bf1b79f0e5e22bc4
Rios Maël <mael.rios@telecom-paris.fr>
2024-04-14 17:04:20 +0200

    dashWingUi

--- Scenes/dash_wing_ui.gd
+++ Scenes/dash_wing_ui.gd
@@ -1,18 +1,26 @@
 extends Control
 
+const SPRITE_SCALE = 4
 var panelContainer:HBoxContainer
 var panels
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	panelContainer = $wings/HBoxContainer
 	panels = panelContainer.get_children()
 	for panel in panels:
 		var sprite = panel.get_node("Sprite2D")
-		var spriteSize = sprite.get_texture().get_size()
+		sprite.set_scale(Vector2(SPRITE_SCALE, SPRITE_SCALE))
+		var spriteSize = sprite.get_texture().get_size()*SPRITE_SCALE
 		panel.set_custom_minimum_size(Vector2(spriteSize.x, spriteSize.y))
 		sprite.set_position(Vector2(spriteSize.x/2, spriteSize.y/2))
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_dash(dashNum:int):
+	for i in range(panels.size()):
+		var panel = panels[i]
+		var sprite = panel.get_node("Sprite2D")
+		if i < dashNum:
+			sprite.modulate.a = 1.0
+		else:
+			sprite.modulate.a = 0.2
