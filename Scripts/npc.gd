46d5ebf992ef90cdc7e2cd3c6da7fa426809cb80
Telo <telo.philippe@telecom-paris.fr>
2024-04-15 09:25:52 +0200

    Added stats and gift option

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -6,6 +6,12 @@ extends Node2D
 var next_state = null
 
 var dialogIndex = 0;
+var firstDialog = true
+var firstGift = true
+
+var kiss = 0
+var marry = 0
+var kill = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,13 +23,21 @@ func _ready():
 func talk():
 	Dialogic.timeline_ended.connect(ended)
 	player.block_movements()
+	if not firstDialog:
+		transition()
+	firstDialog = false
+	next_state = dialogs[dialogIndex]
 	Dialogic.start(dialogs[dialogIndex])
 
+func gift():
+	if firstGift:
+		firstGift = false
+		Dialogic.start("Gift1")
+	else:
+		Dialogic.start("Gift2")
 func ended():
 	player.enable_movements()
-	transition()
 	Dialogic.timeline_ended.disconnect(ended)
 
 func transition(): #This will be overriden
 	dialogIndex = (dialogIndex + 1) % dialogs.size()
-	next_state = dialogs[dialogIndex]
