745739cc3457330569b8a150363a98f940fe3f4f
Rios Maël <mael.rios@telecom-paris.fr>
2024-04-15 21:16:01 +0200

    Can now modify char stats after corvee

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -106,4 +106,28 @@ func caveEvent(event):
 	if(event == 'nothing'):
 		print("nothing")
 
-
+func add_to_stat(statName, value):
+	if statName == 'kiss':
+		kiss += value
+		if(kiss > 100):
+			kiss = 100
+		if(kiss < 0):
+			kiss = 0
+		print("Kiss for: ", name, ' is now', kiss, ' and was', kiss - value)
+	elif statName == 'marry':
+		marry += value
+		if(marry > 100):
+			marry = 100
+		if(marry < 0):
+			marry = 0
+		print("Marry for: ", name, ' is now', kiss, ' and was', kiss - value)
+	elif statName == 'kill':
+		kill += value
+		if(kill > 100):
+			kill = 100
+		if(kill < 0):
+			kill = 0
+		print("kill for: ", name, ' is now', kiss, ' and was', kiss - value)
+	else:
+		print("Stat not found")
+		return
--- Scripts/teleported_view_scene.gd
+++ Scripts/teleported_view_scene.gd
@@ -76,7 +76,8 @@ func dialog_finished():
 	player.onCorveeFinished()
 
 func addStat(statname, NPCname, value):
-	pass
+	var npc = get_tree().get_root().get_node("mainScene").get_node(NPCname)
+	npc.add_to_stat(statname, value * 10)
 
 func addObject(objectName):
 	pass
