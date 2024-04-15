8bb9948e44919fdd9da72b3c3e916ade66c1a5c6
ippieh <pierina100a@gmail.com>
2024-04-15 14:42:23 +0200

    commit before pull

---
+++ Scripts/DoloreNPC.gd
@@ -0,0 +1,32 @@
+extends NPC
+
+@export var has_waited = false
+@export var has_red_slip = false
+
+func _ready():
+	dialogs = [
+		"AzazaelMeet", # 0
+		"AzazaelWait", # 1
+		"AzazaelMeet2",# 2
+		"AzazaelMeet3",# 3
+		"AzazaelZouk", # 4
+		"RedSlip",     # 5
+		"RedSlip2"     # 6
+	]
+	kiss = 20
+	marry = 30
+	kill = 50
+
+
+func transition():
+	if dialogIndex < 0:
+		dialogIndex = 0
+	elif dialogIndex == 0:
+		dialogIndex = 1
+	elif dialogIndex == 1:
+		if has_waited:
+			dialogIndex = 2
+		else:
+			dialogIndex = 1
+	elif dialogIndex == 2:
+		dialogIndex = 3
